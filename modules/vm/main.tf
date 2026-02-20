7f871bef32a158331f6963549775a5ee1b3bcb10
Pravin Singh <71260633+Pravin-P-Singh@users.noreply.github.com>
2026-02-20 10:08:41 +0530

    Create Azure Linux VM resource in main.tf
    Add configuration for Azure Linux virtual machine.

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,1 +1,17 @@
+resource "azurerm_linux_virtual_machine" "vm" {
 
+  name                = var.vm_name
+  resource_group_name = var.resource_group
+  location            = var.location
+  size                = var.vm_size
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    var.nic_id
+  ]
+
+  admin_password = "Password@1234"
+
+  disable_password_authentication = false
+}
